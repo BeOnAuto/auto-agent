dedcabe5be26c384b7926c0c1e59b349adc05af4
Sam Hatoum <samhatoum@users.noreply.github.com>
2026-02-07 21:58:05 -0800

    chore(job-graph-processor): export createGraphProcessor from index

--- packages/job-graph-processor/src/index.ts
+++ packages/job-graph-processor/src/index.ts
@@ -2,6 +2,7 @@ export { applyPolicy } from './apply-policy';
 export type { FailurePolicy, GraphState, JobGraphEvent, JobStatus } from './evolve';
 
 export { evolve, getReadyJobs, getTransitiveDependents, initialState, isGraphComplete } from './evolve';
+export { createGraphProcessor } from './graph-processor';
 export type { Job } from './graph-validator';
 export { validateGraph } from './graph-validator';
 export { classifyJobEvent, handleJobEvent, isJobFailure, parseCorrelationId } from './handle-job-event';
